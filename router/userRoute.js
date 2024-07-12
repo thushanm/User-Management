aca3a055953d2e41166a51dc8a0aea1c0adacd3d
thushanm <122750947+thushanm@users.noreply.github.com>
2024-07-12 15:16:41 +0530

    get all data oky

--- router/userRoute.js
+++ router/userRoute.js
@@ -4,5 +4,7 @@ const userController = require("../controller/userController")
 const userRoute = express.Router()
 
 userRoute.route('/').post(userController.addUser)
+userRoute.route('/').get(userController.getAllUsers)
+
 
 module.exports=userRoute;
