48b675a91e9e85bfaa422e3ead454fb83044f7d2
thushanm <122750947+thushanm@users.noreply.github.com>
2024-07-14 14:34:49 +0530

    bug fixed

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const userRoute = require('../backEnd/router/userRoute')
+const cors = require('cors');
 
 const app=express()
+app.use(cors());
 app.use(express.json())
 app.use('/api/v1/user',userRoute)
 
